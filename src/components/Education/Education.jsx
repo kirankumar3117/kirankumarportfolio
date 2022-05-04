8e3b68d12a9166f37f04da725b3f3f19bf57a950
kiran <ch.kirankumar311@gmail.com>
2022-05-05 00:49:26 +0530

    education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "./education.css"
+import DATA from "./edudata.json"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 const Education = () => {
+
+    useEffect(()=>{
+      Aos.init({duration:2000})
+    },[])
+ 
   return (
-    <div>Education</div>
+    <div className="education">
+      <div className="education__container">
+     <div className="education__top">Education</div>
+      <div className="education__grid">{DATA.map(e=>{
+        return (<div className="after__grid" data-aos="fade-up">
+          <div className="education__image"><img src="https://dev-portfolio-template.netlify.app/static/media/eduImgWhite.ae822784.svg"/></div>
+          <div class_name="education__content">
+            <div className="education__year">{e.year}</div>
+            <div className="education__learns">{e.learn}</div>
+            <div className="education__school">{e.school}</div>
+          </div>
+        </div>)
+      })}</div>
+      </div>
+     
+    </div>
   )
 }
 
