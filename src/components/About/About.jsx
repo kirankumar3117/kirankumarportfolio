001695a3e7c4f81f2faa535fab097a4fe6d37756
kiran <ch.kirankumar311@gmail.com>
2022-05-04 21:53:21 +0530

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,7 @@ import MYEDIT from "../../assets/workin.png"
 const About = () => {
   return (
     <div id="about">
+      <div className="about__line"></div>
       <div className="about__top">
         <div className='about__top1'></div>
         <div className='about__top2'></div>
@@ -15,8 +16,8 @@ const About = () => {
     <div className="about__content">
       <div className="about__content1">
         <div className='whoiam'>Who I am</div>
-       <p> My name's kiran kumar. I've recently completed a skill development course from Masai School with strong background in Full-Stack Web Development, Data Structures And Algorithms.</p>       
-        <div>During The Course I Have Done Countless Of Projects Individualy.and Done Three Collaborative Projects. I spent my leisure hours writing codes and blogs. Right now I'm also trying a hand at mastering the skills i have learned, and I love to learn and explore new arenas.</div>
+       <p className="whoiamp"> My name's kiran kumar. I've recently completed a skill development course from Masai School with strong background in Full-Stack Web Development, Data Structures And Algorithms.</p>       
+        <div className="whoiampdiv">During The Course I Have Done Countless Of Projects Individualy.and Done Three Collaborative Projects. I spent my leisure hours writing codes and blogs. Right now I'm also trying a hand at mastering the skills i have learned, and I love to learn and explore new arenas.</div>
 
       </div>
       <div className="about__content2">
